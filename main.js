cfcd48e56f29581ab1cdf2bdc66c39c8e2ed6fa3
Tommy Chiu <txmmychiu@gmail.com>
2024-05-29 13:55:17 -0400

    Update

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ var servers = {};
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
 

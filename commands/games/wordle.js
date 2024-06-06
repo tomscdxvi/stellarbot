305d814ecfe0e6673eadbc78865c2f6a0c1e4061
Tommy Chiu <txmmychiu@gmail.com>
2024-06-05 23:22:47 -0400

    Work on wordle

--- commands/games/wordle.js
+++ commands/games/wordle.js
@@ -1,8 +1,10 @@
+const { EmbedBuilder } = require('discord.js');
 const User = require('../../schemas/User');
 const Cooldown = require('../../schemas/Cooldown');
 
-const words = ["car", "moon"];
+const words = ["moon", "butt", "human"];
 
+/*
 function shuffle(array) {
     let currentIndex = array.length;
   
@@ -14,10 +16,10 @@ function shuffle(array) {
       currentIndex--;
   
       // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
 }
+*/
 
 module.exports = {
     run: async({ interaction }) => {
@@ -28,7 +30,57 @@ module.exports = {
             });
             return;
         };
+        
+        try {
+            await interaction.deferReply();
 
+            let user = await User.findOne({
+                userId: interaction.member.id
+            });
+
+            let embed = new EmbedBuilder()
+                .setTitle("Guess the word!")
+                //.setDescription(`Guess the word...`)
+                .setFooter({ text: `StellarBot` })
+                .setColor("#00eaff")
+
+            interaction.editReply({ embeds: [embed]});
+
+            const filter = m => m.author.id === interaction.user.id
+
+            // Randomize words array and pop the last word in the array into word   
+            words.sort(() => 0.5 - Math.random());
+            const word = [words.pop()];
+
+            let wordle = new Map();
+            
+
+            interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors:['time'] })
+                .then(async (collected) => {
+                    if(collected.first().content == (word)) {
+                        interaction.channel.send(`Correct!`);
+
+                        //cooldown.endsAt = Date.now() + 5_000;
+                        //await cooldown.save();
+                    } else {
+                        interaction.channel.send("Not that one!");
+
+                        //cooldown.endsAt = Date.now() + 5_000;
+                        //await cooldown.save();
+                    }
+            }).catch(async () => {
+                await interaction.editReply({ content:'You did not guess in time!', embeds: []});
+
+                //cooldown.endsAt = Date.now() + 5_000;
+                //await cooldown.save();
+            });
+
+            if(!user) {
+                user = new User({ userId });
+            }
+        } catch(error) {
+            console.log(`Error: ${error}`);
+        }
     },
     data: {
         name: 'wordle',
